ac358494198ea4eeba5d485ee8ac7e71f99b3ca7
Krzysztof Mazur <ruzam.pl@gmail.com>
2021-05-06 16:20:40 +0200

    Dump removed

--- DependencyInjection/BaseExtension.php
+++ DependencyInjection/BaseExtension.php
@@ -89,11 +89,8 @@ abstract class BaseExtension extends Extension
     public function mapParameters(ContainerBuilder $container, array $config, string $prefix)
     {
 
-        dump($config);
-
         if (!isset($config[self::CONFIG_KEY_RESOURCES]) || !is_array($config[self::CONFIG_KEY_RESOURCES])) {
-//            throw new \Exception('Missing resources array. Check bundle configuration.');
-            return;
+            throw new \Exception('Missing resources array. Check bundle configuration.');
         }
 
         $configParameterName = $prefix.static::DOT.static::CONFIG_KEY_CONFIG;
